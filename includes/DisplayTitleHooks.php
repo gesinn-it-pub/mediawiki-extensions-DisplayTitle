3b2d363c8902c33a1c9881d0ad1692bdb1db89fe
Luke Eversfield <luke.eversfield@gesinn.it>
2023-09-19 16:45:35 +0200

    Revert last commit

--- includes/DisplayTitleHooks.php
+++ includes/DisplayTitleHooks.php
@@ -56,21 +56,17 @@ class DisplayTitleHooks
 	 */
 	public static function onPageSaveComplete(WikiPage $wikiPage, MediaWiki\User\UserIdentity $user, string $summary, int $flags, MediaWiki\Revision\RevisionRecord $revisionRecord, MediaWiki\Storage\EditResult $editResult)
 	{
-		$displaytitle = '';
-		$found = self::getDisplayTitle($wikiPage->getTitle()->getSubjectPage(), $displaytitle);
-		if ($found) {
-			$incomingLinks = self::getIncomingLinks($wikiPage->getTitle());
-			$jobs = [];
-			foreach ($incomingLinks as $row) {
-				$jobs[] = new DisplayTitlePurgeIncomingLinksJob([
-					'pageid' => $row->page_id
-				]);
-			}
-			if($jobs) {
-				JobQueueGroup::singleton()->lazyPush($jobs);
-			}
-			return true;
+		$incomingLinks = self::getIncomingLinks($wikiPage->getTitle());
+		$jobs = [];
+		foreach ($incomingLinks as $row) {
+			$jobs[] = new DisplayTitlePurgeIncomingLinksJob([
+				'pageid' => $row->page_id
+			]);
 		}
+		if($jobs) {
+			JobQueueGroup::singleton()->lazyPush($jobs);
+		}
+		return true;
 	}
 
 	/**
